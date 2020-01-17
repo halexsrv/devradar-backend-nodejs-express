3d30835c7ac272610ff82a6c76e4ff072af94179
Alex Vitorino <halexsrv@gmail.com>
2020-01-17 01:40:45 -0300

    add axios module and setup devs init route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
+routes.post('/devs', async (request, response) => {
+  const { github_username } = request.body;
+
+  const apiResponse = await axios.get(
+    `https://api.github.com/users/${github_username}`,
+  );
+
+  console.log(apiResponse.data);
+
   return response.json({ message: 'test' });
 });
 
