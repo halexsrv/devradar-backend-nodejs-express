4dec977a415c1e4361f3a4d38dabb1d2214d438b
Alex Vitorino <halexsrv@gmail.com>
2020-01-17 01:03:15 -0300

    add suport for json in express request

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (request, response) => {
   return response.json({ message: 'test' });
 });
